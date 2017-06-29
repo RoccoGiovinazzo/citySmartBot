5da4ab161d40f57466f8f3014f62d6c59ea36e1f
Rocco Giovinazzo <giovinazzorocco@hotmail.it>
2017-06-29 16:53:32 +0200

    webPage added

--- smartBot/settings.py
+++ smartBot/settings.py
@@ -64,8 +64,7 @@ ROOT_URLCONF = 'smartBot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates', 'allauth') ,
-                  os.path.join(BASE_DIR, 'webPage', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
